92b791d70860c69d9bb52707401b479ff67b1f65
Joey Clover <joey.clover@theladbiblegroup.com>
2016-03-26 14:27:40 +0000

    cleanup

--- liberty/init.go
+++ liberty/init.go
@@ -2,37 +2,54 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 )
 
 // PerformInit asks the user a number of questions to setup the project
 // It creates a liberty (JSON) file with these settings (if provided)
 func PerformInit() {
 	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("-- Welcome to Liberty! --")
+	libertyData := make(map[string]interface{})
+
+	fmt.Println("*** Welcome to Liberty! ***")
 	fmt.Println("We are going to ask you a few questions about your project.")
 	fmt.Print("Is that okay? (Y/N): ")
 
 	shouldAskQuestions := requestPermission(scanner)
 
-	if !shouldAskQuestions {
-		fmt.Print("You've now been liberated. Enjoy your freedom!")
-		return
+	if shouldAskQuestions {
+		projectData := make(map[string]string)
+
+		fmt.Print("Project Title: ")
+		projectData["title"] = fetchInput(scanner)
+
+		fmt.Print("Project Description: ")
+		projectData["description"] = fetchInput(scanner)
+
+		fmt.Print("Author Name: ")
+		projectData["author"] = fetchInput(scanner)
+
+		libertyData["project"] = projectData
 	}
 
-	// Assuming yes here
-	fmt.Print("Project Title: ")
-	projectTitle := fetchInput(scanner)
+	libertyData["dependencies"] = make(map[string]string)
+	generateLibertyFile(&libertyData)
+}
 
-	fmt.Print("Project Description: ")
-	projectDesc := fetchInput(scanner)
+func generateLibertyFile(data *map[string]interface{}) {
+	jsonData, marshalErr := json.MarshalIndent(data, "", "\t")
+	if marshalErr != nil {
+		panic(marshalErr)
+	}
 
-	fmt.Print("Author Name: ")
-	authorName := fetchInput(scanner)
-
-	fmt.Printf("%s + %s + %s", projectTitle, projectDesc, authorName)
+	if err := ioutil.WriteFile("liberty.json", jsonData, 0644); err != nil {
+		panic(err)
+	}
 }
 
 // Fetches input and handles any errors
@@ -48,43 +65,14 @@ func fetchInput(scanner *bufio.Scanner) string {
 }
 
 // Ask the user for a YES/NO response
-// It supports fuzzy responses (y/n, yes/no, YES/NO)
+// If the users response starts with y, the return value is true, else, false.
 // It returns a boolean that represents the users response
 func requestPermission(scanner *bufio.Scanner) bool {
+	text := fetchInput(scanner)
 
-	okayResponses := []string{"y", "Y", "yes", "Yes", "YES"}
-	nokayResponses := []string{"n", "N", "no", "No", "NO"}
-
-	scanner.Scan()
-
-	err := scanner.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	text := scanner.Text()
-	if containsString(okayResponses, text) {
+	if len(text) > 0 && strings.ToUpper(text[:1]) == "Y" {
 		return true
-	} else if containsString(nokayResponses, text) {
-		return false
-	} else {
-		fmt.Print("Please enter either [Y/N] to continue:")
-		return requestPermission(scanner)
 	}
-}
 
-// posString returns the first index of element in slice.
-// If slice does not contain element, returns -1.
-func posString(slice []string, element string) int {
-	for index, elem := range slice {
-		if elem == element {
-			return index
-		}
-	}
-	return -1
-}
-
-// containsString returns true iff slice contains element
-func containsString(slice []string, element string) bool {
-	return !(posString(slice, element) == -1)
+	return false
 }
