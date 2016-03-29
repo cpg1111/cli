f4c3fdd9dbc1a9a72fe1236780128f2d6e4aa4c6
Joel <joel.lord@hotmail.co.uk>
2016-03-29 06:15:59 -0400

    Update help file to include liberty get

--- help.go
+++ help.go
@@ -15,24 +15,28 @@ type Arguments []Argument
 func getHelpArguments() Arguments {
 	var arguments = Arguments{
 		Argument{
-			Name: "liberty init",
+			Name:  "liberty init",
 			Usage: "Initializes your project with a liberty file. If you provide the tool with additional information,\nit will enable other developers to find your code using our website. However, we don't store anything more than we ask.",
 		},
 		Argument{
-			Name: "liberty install",
+			Name:  "liberty install",
 			Usage: "Downloads and installs any dependencies in your liberty file.\nThe'lib' dependencies will be installed in a directory names _libs at the root of your project.",
 		},
 		Argument{
-			Name: "liberty update",
+			Name:  "liberty update",
 			Usage: "Updates liberty to the latest stable version.",
 		},
 		Argument{
-			Name: "liberty version",
+			Name:  "liberty version",
 			Usage: "Displays the version that is in the $PATH. Also displays the build architecture.",
 		},
 		Argument{
-			Name: "liberty list",
+			Name:  "liberty list",
 			Usage: `Displays a list of the installed packages and their versions. Also displays whether they are 'binary' dependencies or 'lib' dependencies.`,
+		},
+		Argument{
+			Name:  "liberty get",
+			Usage: "Download and install a single package to your _libs directory and update your liberty file.",
 		},
 	}
 	return arguments
